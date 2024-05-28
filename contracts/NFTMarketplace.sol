32d5a2f87bf056116b77cd689250bbc2dcfdcaa1
Анисимов Евгений Владимирович <AnisimovEV@gnivc.ru>
2024-05-29 00:05:45 +0300

    adding new contarct

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -1,71 +1,75 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NFTMarketplace is ERC721 {
-    address owner;
-    uint256 private _tokenIdCounter;
+contract NFTMarketplace is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
-    struct Sale {
-        address seller;
+    struct NFT {
+        uint256 id;
+        string uri;
         uint256 price;
+        address payable creator;
     }
 
-    mapping(uint256 => Sale) public tokenSales;
-    uint256[] public tokensForSale;
+    mapping(uint256 => NFT) public nfts;
+    mapping(uint256 => bool) public listedMap;
 
-    constructor() ERC721("ZhendyNFT", "ZNFT") {
-        owner = msg.sender;
+    event Minted(uint256 indexed tokenId, address indexed creator, string uri, uint256 price);
+    event Bought(uint256 indexed tokenId, address indexed buyer, uint256 price);
+    event Listed(uint256 indexed tokenId, uint256 price);
+
+    constructor() ERC721("NFT Marketplace", "NFTM") {}
+
+    function mintNFT(string memory tokenURI, uint256 price) public {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        nfts[newItemId] = NFT(newItemId, tokenURI, price, payable(msg.sender));
+        listedMap[newItemId] = true;
+
+        emit Minted(newItemId, msg.sender, tokenURI, price);
     }
 
-    modifier onlyOwner {
-        require(msg.sender == owner, "Caller is not the owner");
-        _;
+    function buyNFT(uint256 tokenId) public payable {
+        require(listedMap[tokenId], "NFT not listed for sale");
+        NFT memory nft = nfts[tokenId];
+        require(msg.value >= nft.price, "Insufficient funds");
+
+        address previousOwner = ownerOf(tokenId);
+        _transfer(previousOwner, msg.sender, tokenId);
+        payable(previousOwner).transfer(msg.value);
+
+        listedMap[tokenId] = false;
+
+        emit Bought(tokenId, msg.sender, nft.price);
     }
 
-    function safeMint(address to) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter;
-        _tokenIdCounter += 1;
-        _safeMint(to, tokenId);
+    function listNFT(uint256 tokenId, uint256 price) public {
+        require(ownerOf(tokenId) == msg.sender, "Only owner can list");
+        nfts[tokenId].price = price;
+        listedMap[tokenId] = true;
+
+        emit Listed(tokenId, price);
     }
 
-    function listTokenForSale(uint256 tokenId, uint256 price) public {
-        require(ownerOf(tokenId) == msg.sender, "You are not the owner of this token");
-        require(price > 0, "Price must be greater than zero");
+    function getAllNFTs() public view returns (NFT[] memory) {
+        uint256 totalNFTs = _tokenIds.current();
+        NFT[] memory allNFTs = new NFT[](totalNFTs);
 
-        tokenSales[tokenId] = Sale(msg.sender, price);
-        tokensForSale.push(tokenId);
-        approve(address(this), tokenId);
-    }
-
-    function buyToken(uint256 tokenId) public payable {
-        Sale memory sale = tokenSales[tokenId];
-        require(sale.price > 0, "This token is not for sale");
-        require(msg.value >= sale.price, "Insufficient funds");
-
-        address seller = sale.seller;
-
-        // Transfer token to buyer
-        _transfer(seller, msg.sender, tokenId);
-
-        // Transfer funds to seller
-        payable(seller).transfer(msg.value);
-
-        // Clear the sale
-        delete tokenSales[tokenId];
-
-        // Remove token from tokensForSale array
-        for (uint256 i = 0; i < tokensForSale.length; i++) {
-            if (tokensForSale[i] == tokenId) {
-                tokensForSale[i] = tokensForSale[tokensForSale.length - 1];
-                tokensForSale.pop();
-                break;
-            }
+        for (uint256 i = 0; i < totalNFTs; i++) {
+            uint256 tokenId = i + 1; // Token IDs start from 1
+            NFT storage nft = nfts[tokenId];
+            allNFTs[i] = nft;
         }
-    }
 
-    function getTokensForSale() public view returns (uint256[] memory) {
-        return tokensForSale;
+        return allNFTs;
     }
 }
